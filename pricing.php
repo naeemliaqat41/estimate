5cc73f74a625c04bf029d09ec34ef6a270e2520a
naeem4liaqat <naeemliaqat41@yahoo.com>
2021-07-05 19:24:52 +0500

    new repo

---
+++ pricing.php
@@ -0,0 +1,147 @@
+<?php include 'inc/navbar.php' ?>
+<!-- BANNER -->
+<div class="section banner-page">
+   <div class="container">
+      <div class="row">
+         <div class="col-sm-12 col-md-12">
+            <div class="title-page">PRICING SUMMARY</div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Pages -->
+<div class="section">
+   <div class="container">
+      <div class="row">
+         <div class="col-sm-12 col-md-12">
+            <h4> You will find below an overview of our basic pricing, and we will always provide a fixed quote for your project.</h4>
+            <p> Every construction project is different, so we can list a complete price list without knowing the project</p>
+            <br>
+            <div class="col-sm-4 col-md-4">
+               <!-- ITEM 1 -->
+               <div class="panel panel-pricing">
+                  <header class="panel-heading">
+                     <h3>SMALL PROJECTS</h3>
+                     <div class="price">
+                        <sup>From</sup>$65.00<small></small>
+                     </div>
+                  </header>
+                  <div class="panel-body">
+                     <table class="table">
+                        <tbody>
+                           <tr>
+                              <td>Detached Garages</td>
+                           </tr>
+                           <tr>
+                              <td>Extensions</td>
+                           </tr>
+                           <tr>
+                              <td>New Build House</td>
+                           </tr>
+                           <tr>
+                              <td>Basement Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Garage Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Loft Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Bespoke Projects</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+                  <div class="panel-footer">
+                     <a href="contact.php" class="btn btn-secondary">CONTACT US</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+               <!-- ITEM 2 -->
+               <div class="panel panel-pricing best">
+                  <header class="panel-heading">
+                     <h3>MEDIUM PROJECTS</h3>
+                     <div class="price">
+                        <sup>From</sup>$120.00<small></small>
+                     </div>
+                  </header>
+                  <div class="panel-body">
+                     <table class="table">
+                        <tbody>
+                           <tr>
+                              <td>Detached Garages</td>
+                           </tr>
+                           <tr>
+                              <td>Extensions</td>
+                           </tr>
+                           <tr>
+                              <td>New Build House</td>
+                           </tr>
+                           <tr>
+                              <td>Basement Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Garage Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Loft Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Bespoke Projects</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+                  <div class="panel-footer">
+                     <a href="contact.php" class="btn btn-secondary">CONTACT US</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+               <!-- ITEM 3 -->
+               <div class="panel panel-pricing">
+                  <header class="panel-heading">
+                     <h3>LARGE PROJECTS</h3>
+                     <div class="price">
+                        <sup>From</sup>$150.00<small></small>
+                     </div>
+                  </header>
+                  <div class="panel-body">
+                     <table class="table">
+                        <tbody>
+                           <tr>
+                              <td>Detached Garages</td>
+                           </tr>
+                           <tr>
+                              <td>Extensions</td>
+                           </tr>
+                           <tr>
+                              <td>New Build House</td>
+                           </tr>
+                           <tr>
+                              <td>Basement Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Garage Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Loft Conversions</td>
+                           </tr>
+                           <tr>
+                              <td>Bespoke Projects</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+                  <div class="panel-footer">
+                     <a href="contact.php" class="btn btn-secondary">CONTACT US</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<?php include 'inc/footer.php' ?>
---
+++ sample-work.php
@@ -0,0 +1,54 @@
+<?php include 'inc/navbar.php' ?>
+<!-- BANNER -->
+<div class="section banner-page">
+   <div class="container">
+      <div class="row">
+         <div class="col-sm-12 col-md-12">
+            <div class="title-page">Sample Work</div>
+         </div>
+      </div>
+   </div>
+</div>
+<div class="section">
+   <div class="container">
+      <div class="row">
+         <div class="col-sm-12 col-md-12">
+            <h4> You will find below an overview of our Sample work, and we will always provide a fixed quote for your project.</h4>
+            <p> Every construction project is different, so we can Provide a sample work drawing without knowing the project</p>
+            <br>
+            <div class="col-sm-2 col-md-2"></div>
+            <div class="col-sm-4 col-md-4">
+               <!-- ITEM 1 -->
+               <div class="panel panel-pricing">
+                  <header class="panel-heading">
+                     <h3>PDF SAMPLE FILE</h3>
+                  </header>
+                  <div class="panel-body">
+                      <i class="fa fa-file-pdf-o fa-5x text-danger"></i>
+                  </div>
+                  <div class="panel-footer">
+                  <a class="btn btn-secondary" href="front_assets/images/sample/sample.pdf" download>  <i class="fa fa-file-pdf-o fa-lg"></i>  PDF SAMPLE FILE</a>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col-sm-4 col-md-4">
+               <!-- ITEM 3 -->
+               <div class="panel panel-pricing">
+                  <header class="panel-heading">
+                     <h3>EXCEL SAMPLE FILE</h3>
+                  </header>
+                  <div class="panel-body">
+                     <i class="fa fa-file-excel-o fa-5x text-success"></i>
+                  </div>
+                  <div class="panel-footer">
+                     <a class="btn btn-secondary" href="front_assets/images/sample/sample estimation sheet.xlsx" download>  <i class="fa fa-file-excel-o fa-lg"></i> EXCEL SAMPLE FILE</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php include 'inc/footer.php' ?>
